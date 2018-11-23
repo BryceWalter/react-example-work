3146688e43341fc815e151ae515bd19e9ea0d155
Bryce Walter <brycewalterparker@gmail.com>
2018-11-23 16:07:54 -0500

    ✨ First Page and project structure

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Link from 'next/link'
+
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Aloha</h1>
+            </div>
+        );
+    }
+}
+
+export default Index;
